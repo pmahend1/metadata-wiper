523fc92cd00bf130f61c01cd9fdfdb1475deea05
pmahend1 <prateek.mahendrakar@gmail.com>
2025-11-19 22:10:54 -0500

    Bumped version to 1.3(13) with mapping files generation change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,15 +15,15 @@ android {
         applicationId = "com.prateekmahendrakar.metadatawiper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.2"
+        versionCode = 13
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
