9d7129bf64b979492942d6ee60dcf9d5caa26757
pmahend1 <prateek.mahendrakar@gmail.com>
2026-01-06 22:50:02 -0500

    Added more formatted values

--- app/src/main/java/com/prateekmahendrakar/metadatawiper/utils/ExifUtils.kt
+++ app/src/main/java/com/prateekmahendrakar/metadatawiper/utils/ExifUtils.kt
@@ -1,5 +1,7 @@
 package com.prateekmahendrakar.metadatawiper.utils
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.exifinterface.media.ExifInterface
 
 fun Double.formatted(): String {
@@ -41,15 +43,17 @@ fun getRemovableExifTags(): Array<String> {
     return getAllExifTags().filterNot { it in essentialTags }.toTypedArray()
 }
 
+@SuppressLint("RestrictedApi")
 fun getFormattedExifValue(exif: ExifInterface, tag: String): String {
     val value = exif.getAttribute(tag)
+    Log.d("EXIF", "TAG: $tag, VALUE: $value")
     val textOutput = when (tag) {
         ExifInterface.TAG_IMAGE_LENGTH,
         ExifInterface.TAG_IMAGE_WIDTH,
         ExifInterface.TAG_PIXEL_X_DIMENSION,
         ExifInterface.TAG_PIXEL_Y_DIMENSION -> "${value}px"
 
-        ExifInterface.TAG_ORIENTATION -> when (value?.toIntOrNull()) {
+        ExifInterface.TAG_ORIENTATION, ExifInterface.TAG_THUMBNAIL_ORIENTATION -> when (value?.toIntOrNull()) {
             ExifInterface.ORIENTATION_NORMAL -> "Normal"
             ExifInterface.ORIENTATION_FLIP_HORIZONTAL -> "Mirror Horizontal"
             ExifInterface.ORIENTATION_ROTATE_180 -> "Rotate 180"
@@ -102,13 +106,13 @@ fun getFormattedExifValue(exif: ExifInterface, tag: String): String {
             ExifInterface.LIGHT_SOURCE_ISO_STUDIO_TUNGSTEN -> "ISO Studio Tungsten"
             ExifInterface.LIGHT_SOURCE_OTHER -> "Other"
             ExifInterface.LIGHT_SOURCE_UNKNOWN -> "Unknown"
-            else -> "Undefined"
+            else -> "Unknown"
         }
 
         ExifInterface.TAG_COLOR_SPACE -> when (value?.toIntOrNull()) {
             ExifInterface.COLOR_SPACE_S_RGB -> "sRGB"
             ExifInterface.COLOR_SPACE_UNCALIBRATED -> "Uncalibrated"
-            else -> "Undefined"
+            else -> ""
         }
 
         ExifInterface.TAG_METERING_MODE -> when (value?.toShortOrNull()) {
@@ -136,7 +140,7 @@ fun getFormattedExifValue(exif: ExifInterface, tag: String): String {
 
         ExifInterface.TAG_BRIGHTNESS_VALUE -> when (val brightness = exif.getAttributeDouble(tag, 0.0)) {
             0.0 -> "Unknown"
-            else -> "$brightness"
+            else -> brightness.formatted()
         }
 
         ExifInterface.TAG_Y_RESOLUTION,
@@ -169,20 +173,156 @@ fun getFormattedExifValue(exif: ExifInterface, tag: String): String {
             else -> "Normal"
         }
 
-        ExifInterface.TAG_COMPONENTS_CONFIGURATION -> when (value) {
-            "4560" -> "RGB Uncompressed"
-            "1230" -> "YCbCr"
-            else -> value ?: ""
+        ExifInterface.TAG_COMPONENTS_CONFIGURATION -> {
+            val bytes = exif.getAttributeBytes(ExifInterface.TAG_COMPONENTS_CONFIGURATION)
+
+            val result = bytes?.take(4)?.joinToString(" ") {
+                when (it.toInt()) {
+                    1 -> "Y"
+                    2 -> "Cb"
+                    3 -> "Cr"
+                    4 -> "R"
+                    5 -> "G"
+                    6 -> "B"
+                    else -> "-"
+                }
+            }
+            return result ?: ""
         }
 
-        ExifInterface.TAG_APERTURE_VALUE -> when (val apertureValue = exif.getAttributeDouble(tag, 0.0)) {
-            apertureValue -> apertureValue.formatted()
-            else -> "Unknown"
+        ExifInterface.TAG_APERTURE_VALUE -> {
+            val apertureValue = exif.getAttributeDouble(tag, 0.0)
+            apertureValue.formatted()
         }
 
         ExifInterface.TAG_DIGITAL_ZOOM_RATIO -> when (val doubleValue = exif.getAttributeDouble(tag, 0.0)) {
             doubleValue -> doubleValue.formatted()
             else -> "Unknown"
+        }
+
+        ExifInterface.TAG_SCENE_TYPE -> when (value?.toShortOrNull()) {
+            ExifInterface.SCENE_TYPE_DIRECTLY_PHOTOGRAPHED -> "Directly Photographed"
+            else -> "Unknown"
+        }
+
+        ExifInterface.TAG_EXPOSURE_MODE -> when (value?.toShortOrNull()) {
+            ExifInterface.EXPOSURE_MODE_AUTO -> "Auto"
+            ExifInterface.EXPOSURE_MODE_MANUAL -> "Manual"
+            ExifInterface.EXPOSURE_MODE_AUTO_BRACKET -> "Auto Bracket"
+            else -> ""
+        }
+
+        ExifInterface.TAG_EXPOSURE_TIME, ExifInterface.TAG_SUBSEC_TIME_ORIGINAL, ExifInterface.TAG_SUBSEC_TIME_DIGITIZED -> {
+            val exposureTime = exif.getAttributeDouble(tag, 0.0) * 1000
+            "${exposureTime.formatted()} milliseconds"
+        }
+
+        ExifInterface.TAG_EXPOSURE_PROGRAM -> when (value?.toShortOrNull()) {
+            ExifInterface.EXPOSURE_PROGRAM_MANUAL -> "Manual"
+            ExifInterface.EXPOSURE_PROGRAM_NORMAL -> "Normal"
+            ExifInterface.EXPOSURE_PROGRAM_APERTURE_PRIORITY -> "Aperture priority"
+            ExifInterface.EXPOSURE_PROGRAM_SHUTTER_PRIORITY -> "Shutter priority"
+            ExifInterface.EXPOSURE_PROGRAM_CREATIVE -> "Creative program (depth of field)"
+            ExifInterface.EXPOSURE_PROGRAM_ACTION -> "Action program (fast shutter speed)"
+            ExifInterface.EXPOSURE_PROGRAM_PORTRAIT_MODE -> "Portrait mode"
+            ExifInterface.EXPOSURE_PROGRAM_LANDSCAPE_MODE -> "Landscape mode"
+            ExifInterface.EXPOSURE_PROGRAM_NOT_DEFINED -> "Not defined"
+            else -> "Not defined"
+        }
+
+        ExifInterface.TAG_SCENE_CAPTURE_TYPE -> when (value?.toShortOrNull()) {
+            ExifInterface.SCENE_CAPTURE_TYPE_LANDSCAPE -> "Landscape"
+            ExifInterface.SCENE_CAPTURE_TYPE_PORTRAIT -> "Portrait"
+            ExifInterface.SCENE_CAPTURE_TYPE_NIGHT -> "Night scene"
+            ExifInterface.SCENE_CAPTURE_TYPE_STANDARD -> "Standard"
+            else -> "Standard"
+        }
+
+        ExifInterface.TAG_GPS_IMG_DIRECTION -> {
+            val gpsImageDirection = exif.getAttributeDouble(tag, 0.0)
+            gpsImageDirection.formatted()
+        }
+
+        ExifInterface.TAG_SHARPNESS -> when (value?.toShortOrNull()) {
+            ExifInterface.SHARPNESS_SOFT -> "Soft"
+            ExifInterface.SHARPNESS_HARD -> "Hard"
+            else -> "Normal"
+        }
+
+        ExifInterface.TAG_GPS_IMG_DIRECTION_REF -> when (value) {
+            ExifInterface.GPS_DIRECTION_MAGNETIC -> "Magnetic direction"
+            else -> "True direction"
+        }
+
+        ExifInterface.TAG_Y_CB_CR_POSITIONING -> when (value?.toShortOrNull()) {
+            ExifInterface.Y_CB_CR_POSITIONING_CO_SITED -> "Co-sited"
+            ExifInterface.Y_CB_CR_POSITIONING_CENTERED -> "Centered"
+            else -> "Centered"
+        }
+
+        ExifInterface.TAG_SUBJECT_DISTANCE_RANGE -> when (value?.toShortOrNull()) {
+            ExifInterface.SUBJECT_DISTANCE_RANGE_MACRO -> "Macro"
+            ExifInterface.SUBJECT_DISTANCE_RANGE_CLOSE_VIEW -> "Close view"
+            ExifInterface.SUBJECT_DISTANCE_RANGE_DISTANT_VIEW -> "Distant view"
+            ExifInterface.SUBJECT_DISTANCE_RANGE_UNKNOWN -> "Unknown"
+            else -> ""
+        }
+
+        ExifInterface.TAG_SENSITIVITY_TYPE -> when (value?.toShortOrNull()) {
+            ExifInterface.SENSITIVITY_TYPE_SOS -> "Standard Output Sensitivity (SOS)"
+            ExifInterface.SENSITIVITY_TYPE_REI -> "Recommended Exposure Index (REI)"
+            ExifInterface.SENSITIVITY_TYPE_ISO_SPEED -> "ISO Speed"
+            ExifInterface.SENSITIVITY_TYPE_SOS_AND_REI -> "SOS and REI"
+            ExifInterface.SENSITIVITY_TYPE_SOS_AND_ISO -> "SOS and ISO Speed"
+            ExifInterface.SENSITIVITY_TYPE_REI_AND_ISO -> "REI and ISO Speed"
+            ExifInterface.SENSITIVITY_TYPE_SOS_AND_REI_AND_ISO -> "SOS, REI, and ISO Speed"
+            ExifInterface.SENSITIVITY_TYPE_UNKNOWN -> "Unknown"
+            else -> "Unknown"
+        }
+
+        ExifInterface.TAG_CUSTOM_RENDERED -> when (value?.toShortOrNull()) {
+            ExifInterface.RENDERED_PROCESS_CUSTOM -> "Custom"
+            ExifInterface.RENDERED_PROCESS_NORMAL -> "Normal"
+            else -> "Unknown"
+        }
+
+        ExifInterface.TAG_SUBJECT_DISTANCE -> when (val doubleValue = exif.getAttributeDouble(tag, 0.0)) {
+            4294967295.0 -> "Infinity"
+            0.0 -> "Unknown"
+            doubleValue -> doubleValue.formatted()
+            else -> ""
+        }
+
+        ExifInterface.TAG_SATURATION -> when (value?.toShortOrNull()) {
+            ExifInterface.SATURATION_LOW -> "Low"
+            ExifInterface.SATURATION_HIGH -> "High"
+            ExifInterface.SATURATION_NORMAL -> "Normal"
+            else -> "Normal"
+        }
+
+        ExifInterface.TAG_XMP -> ""
+
+        ExifInterface.TAG_FOCAL_LENGTH_IN_35MM_FILM -> when (val fValue = value?.toShortOrNull()) {
+            0.toShort() -> "Unknown"
+            fValue -> "${fValue}mm"
+            else -> ""
+        }
+
+        ExifInterface.TAG_FOCAL_LENGTH -> when (val fValue = exif.getAttributeDouble(tag, 0.0)) {
+            0.0 -> "Unknown"
+            fValue -> "${fValue}mm"
+            else -> ""
+        }
+
+        ExifInterface.TAG_SHUTTER_SPEED_VALUE -> when (val shutterSpeed = exif.getAttributeDouble(tag, 0.0)) {
+            0.0 -> "Unknown"
+            shutterSpeed -> shutterSpeed.formatted()
+            else -> ""
+        }
+
+        ExifInterface.TAG_MAX_APERTURE_VALUE -> when (val apertureValue = exif.getAttributeDouble(tag, 0.0)) {
+            apertureValue -> apertureValue.formatted()
+            else -> ""
         }
 
         else -> {
