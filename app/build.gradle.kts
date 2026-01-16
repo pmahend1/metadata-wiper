4af86e23c5d7b95c1d30b8868a0cdec2e9074fd8
pmahend1 <prateek.mahendrakar@gmail.com>
2026-01-15 22:16:39 -0500

    Bumped version to 1.3.0(130)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.prateekmahendrakar.metadatawiper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 120
-        versionName = "1.2.0"
+        versionCode = 130
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
