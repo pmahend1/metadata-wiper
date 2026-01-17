f184f864046c2ed83f5594ca7d4d19b6c36edaab
pmahend1 <prateek.mahendrakar@gmail.com>
2026-01-16 21:47:01 -0500

    Bumped version to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.prateekmahendrakar.metadatawiper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 131
-        versionName = "1.3.1"
+        versionCode = 132
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
